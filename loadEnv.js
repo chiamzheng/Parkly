81bac2ff64a61ea9b06fa3266d5a14682c7c45e7
dinitrophenol <kentpangestu@gmail.com>
2024-11-10 19:45:27 +0800

    deleted

--- loadEnv.js
+++
@@ -1,9 +0,0 @@
-import dotenv from 'dotenv';
-
-dotenv.config(); // Load environment variables from .env file
-
-module.exports = {
-  extra: {
-    SERVER_IP: process.env.SERVER_IP  // Use the env variable 
-  },
-};
