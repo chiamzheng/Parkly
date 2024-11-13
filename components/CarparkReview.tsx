16ffac026b32dd3166445fbd88322a42b0a9cffe
dinitrophenol <kentpangestu@gmail.com>
2024-11-13 17:59:00 +0800

    changed color

--- components/CarparkReview.tsx
+++ components/CarparkReview.tsx
@@ -99,7 +99,7 @@ const styles = StyleSheet.create({
   box:{
     width: 260,
     height: 70,
-    backgroundColor: "yellow",
+    backgroundColor: '#C4BDD8',
     borderRadius: 10,
     alignSelf: 'center',
     marginBottom: 10,
