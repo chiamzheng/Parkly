af64bf71591c0767403b50fe950f93fca73d87f9
lyh0805 <euhanglee@gmail.com>
2024-11-13 23:14:28 +0800

    no more initialization of carparks' reviews

--- backend/src/controller/user_account_manager.js
+++ backend/src/controller/user_account_manager.js
@@ -2,7 +2,7 @@ const UserAccountWrite = require("../repository/database_access/write database/u
 const UserAccountRead = require("../repository/database_access/read database/user_account_read");
 const { sendVerificationEmail } = require('../../src/controller/email_service.js');
 const { password_matches, email_exists, strong_password, email_verified } = require("./user_account_manager_tools");
-const URL = config.SERVER_IP;
+const URL = "http://192.168.0.218";
 
 // async function encrypt_password ( password ) {
 //     const password 
