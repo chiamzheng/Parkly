bcb9da51bb59ce690347fe124a67f4af81c7e8a1
dinitrophenol <kentpangestu@gmail.com>
2024-11-13 21:40:47 +0800

    cleaned up

--- components/Service/dbCarparkService.ts
+++ components/Service/dbCarparkService.ts
@@ -1,11 +1,7 @@
 import Constants from 'expo-constants';
 const URL = Constants.expoConfig?.extra?.SERVER_IP;
-//const BASE_URL = `${URL}/api/carpark` //use this if you are NOT using emulator
+const BASE_URL = `${URL}/api/carpark` //use this if you are NOT using emulator
 
-
-const BASE_URL = 'http://10.0.2.2:8083/api/carpark'; // where your API server is hosted
-//const BASE_URL = 'http://localhost:8083/api/carpark';
-//const BASE_URL = 'http://192.168.1.143:8083/api/carpark';// chiam
 interface Coordinate {
     latitude: number;
     longitude: number;
