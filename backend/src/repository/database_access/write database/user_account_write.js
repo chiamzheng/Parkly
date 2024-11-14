a4efcac97c2b7c963f2ecdcfc91b2dfc9cf9fbbc
Lamnu1902 <tunglamnu@gmail.com>
2024-11-14 14:36:33 +0800

    Fix verifications

--- backend/src/repository/database_access/write database/user_account_write.js
+++ backend/src/repository/database_access/write database/user_account_write.js
@@ -117,14 +117,11 @@ async function add_user_account(user_email, user_password, bookmark_list = []) {
 }
 
 async function verify_account (user_email) {
-    console.log("User verified 1");
-    console.log(user_email);
     const collection = await get_collection(collection_name);
 
     await collection.updateOne(
         { email: user_email }, 
         { $set: { verified: true } }
     );
-    console.log("User verified 2");
 }
 module.exports = { write_email, write_password, write_bookmark_list, add_user_account, verify_account }; //, updateUsername, updatePassword, updateBookmarkList, updateReview };
